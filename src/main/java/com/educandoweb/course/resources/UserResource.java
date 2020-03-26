34680b4f5137e93d180ae29c75eb0dc8db190027
f-mtz <mtz.dev.si@gmail.com>
2020-03-26 01:42:17 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -5,16 +5,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.educandoweb.course.entities.User;
+import com.educandoweb.course.entities.User; //
 
-@RestController
-@RequestMapping(value = "/users")
+@RestController //
+@RequestMapping(value = "/users") //
 public class UserResource {
 	
-	@GetMapping
-	public ResponseEntity<User> findall(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "123456", "12345");
-		return ResponseEntity.ok().body(u);
+	@GetMapping //
+	public ResponseEntity<User> findall(){ //resposta da entidade usuário, encontre todos
+		User u = new User(1L, "Maria", "maria@gmail.com", "123456", "12345"); //instanciando e inicializando dados de um usuario
+		return ResponseEntity.ok().body(u); //resposta da entidade 
 	}
 	
 
